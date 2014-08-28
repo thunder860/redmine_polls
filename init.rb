e89a6c7ea9ff7ccb3c4a0be7feb1a286e08afa9e
Bobby-Work <robert.wisniewski@nsgr-na.com>
2014-08-28 16:21:35 -0400

    extended project menu

--- init.rb
+++ init.rb
@@ -5,5 +5,6 @@ Redmine::Plugin.register :polls do
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
-  menu :application_menu, :polls, { :controller => 'polls', :action => 'index' }, :caption => 'Polls'
+  permission :polls, { :polls => [:index, :vote] }, :public => true
+  menu :project_menu, :polls, { :controller => 'polls', :action => 'index' }, :caption => 'Polls', :after => :activity, :param => :project_id
 end
